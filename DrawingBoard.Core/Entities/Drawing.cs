d4acabd7afa0fc7d907606f2d41e24eb7c001196
enayet329 <mdsakayethossen2@gmail.com>
2024-07-28 07:02:39 +0600

    Add Entities for making diffrent shape

--- DrawingBoard.Core/Entities/Drawing.cs
+++ DrawingBoard.Core/Entities/Drawing.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DrawingBoard.Core.Entities
+﻿public class Drawing
 {
-    public class Drawing
-    {
-       public Guid Id { get; set; }
-        public Guid BoardId { get; set; }
-        public Guid UserId { get; set; }
-        public string? Type { get; set; } // "line", "rectangle", "circle", "text", etc.
-        public string? Color { get; set; } // Json string containing drawing data
-        public DateTime CreatedAt { get; set; }
-    }
+    public Guid Id { get; set; }
+    public Guid BoardId { get; set; }
+    public Guid UserId { get; set; }
+    public string? Type { get; set; } // "line", "rectangle", "circle", "text", "triangle", etc.
+    public string? Color { get; set; } // Hex color code
+
+    // Properties for drawing details
+    public int StartPointX { get; set; }
+    public int StartPointY { get; set; }
+    public int EndPointX { get; set; }
+    public int EndPointY { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public string? Text { get; set; }
+
+    // Properties for triangle vertices
+    public int Vertex1X { get; set; }
+    public int Vertex1Y { get; set; }
+    public int Vertex2X { get; set; }
+    public int Vertex2Y { get; set; }
+    public int Vertex3X { get; set; }
+    public int Vertex3Y { get; set; }
+
+    public DateTime CreatedAt { get; set; }
 }
